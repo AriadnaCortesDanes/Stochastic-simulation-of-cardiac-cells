39c64068780cb5bbd00beb7a78d9fae0ad7866d3
arnaumarzabal <arnaumarzabal@gmail.com>
2024-05-05 16:26:17 +0200

    python animation

--- plotdata.py
+++ plotdata.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import csv
 import sys
+import numpy as np
 
 # Function to read data from CSV file
 def read_csv(filename):
@@ -12,20 +14,59 @@ def read_csv(filename):
                 data[key].append(float(row[key]))
     return data
 
-def plot_main_analysis(data):
+def plot_main_analysis(data,idx):
     fig, axs = plt.subplots(2,sharex=True)
-    axs[0].plot(data['time_0'],data['Volt_0'],label='Volt')
+    axs[0].plot(data[f'time_{idx}'],data[f'Volt_{idx}'],label='Volt')
     axs[0].set(xlabel='Time [ms]', ylabel='Membrane potential [mV]')
     axs[0].legend(loc="upper left")
-    axs[1].plot(data['time_0'],data["M_0"],label="m")
-    axs[1].plot(data['time_0'],data["H_0"],label="h")
-    axs[1].plot(data['time_0'],data["J_0"],label="j")
+    axs[1].plot(data[f'time_{idx}'],data[f"M_{idx}"],label="m")
+    axs[1].plot(data[f'time_{idx}'],data[f"H_{idx}"],label="h")
+    axs[1].plot(data[f'time_{idx}'],data[f"J_{idx}"],label="j")
     axs[1].set(xlabel='Time [ms]', ylabel='Gating variables')
     axs[1].legend(loc="upper left")
     for ax in axs:
         ax.label_outer()
     # plt.legend()
     fig.suptitle("Main analysis")
+    plt.show()
+
+def plot_main_multianalysis(data):
+    # fig, axs = plt.subplots(2,sharex=True)
+    for idx in range(10):
+        plt.plot(data[f'time_{idx}'],data[f'Volt_{idx}'],label='Volt')
+    plt.xlabel("Time [ms]")
+    plt.ylabel("Membrane potential [mV]")
+    plt.show()
+
+def plot_animation(data):
+    n_cells = 100
+    delta_x = 0.1
+    fig, ax = plt.subplots()
+
+    potentials = [ data[f'Volt_{i}'][0] for i in range(n_cells)]
+
+    x = [delta_x*i for i in range(n_cells)]
+
+
+    membrane = ax.plot(x,potentials)[0]
+
+ 
+    ax.set(xlim=[0, delta_x*n_cells], ylim=[-90, 50], xlabel='X [mm]', ylabel='V [mV]')
+    ax.legend()
+
+
+    def update(frame):
+        # for each frame, update the data stored on each artist.
+        # # update the scatter plot:
+        # data = np.stack([x, y]).T
+        # scat.set_offsets(data)
+        # update the line plot:
+        potentials = [ data[f'Volt_{i}'][frame] for i in range(n_cells)]
+        membrane.set_ydata(potentials)
+        return (membrane)
+
+
+    ani = animation.FuncAnimation(fig=fig, func=update, frames=1000, interval=200)
     plt.show()
 
 
@@ -47,7 +88,9 @@ def plot_csv(filename):
 
 def main():
     data = read_csv(f"csv/PointBackupData_mod_2.csv")
-    plot_main_analysis(data)
+    # plot_main_multianalysis(data)
+    # plot_main_analysis(data,sys.argv[1])
+    plot_animation(data)
    
 
 # Example usage
