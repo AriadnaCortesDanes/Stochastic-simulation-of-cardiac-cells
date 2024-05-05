70d33791c6c8e5a689e57774b7ce9c3e5a53514b
Ariadna Cortés Danés <acd200111@gmail.com>
2024-05-06 00:06:26 +0200

    animacio rara

--- 2d-animation.py
+++ 2d-animation.py
@@ -20,27 +20,31 @@ def draw_chess_board(ax):
     board_color1 = 'white'
     board_color2 = 'black'
     patches = []
+    indexos = []
 
     # Create chess board
-    for i in range(8):  # Rows
-        for j in range(8):  # Columns
+    for i in range(1):  # Rows
+        for j in range(100):  # Columns
             color = board_color1 if (i + j) % 2 == 0 else board_color2
             patch = Rectangle((j, i), 1, 1, color=color)
             patches.append(patch)
+            indexos.append((j,i))
             ax.add_patch(patch)
 
-    ax.set_xlim(0, 8)
-    ax.set_ylim(0, 8)
+    ax.set_xlim(0, 100)
+    ax.set_ylim(0, 1)
     ax.set_xticks([])
     ax.set_yticks([])
     
-    return patches
+    return patches,indexos
 
-def update_colors(frame, patches):
+def update_colors(frame, patches, indexos, data):
     # This function is called for each frame of the animation
     np.random.shuffle(patches)  # Shuffle the patches
-    for patch in patches:
-        patch.set_color(np.random.choice(['white', 'black', 'red', 'green', 'blue', 'yellow']))
+    for (patch, (i,j)) in zip(patches,indexos):
+        potential = abs(data[f'Volt_{i}'][frame-1]) # potential = abs(data[f'Volt_{i}_{j}'][frame-1])
+        color = (potential/200, 0, 0)
+        patch.set_color(color)
     return patches
 
 def plot_animation(data):
@@ -52,19 +56,20 @@ def plot_animation(data):
     ax = fig.add_subplot(111)
 
     # Draw chess board and add to tkinter
-    patches = draw_chess_board(ax)
+    patches, indexos = draw_chess_board(ax)
     canvas = FigureCanvasTkAgg(fig, master=root)  # A tk.DrawingArea.
     canvas.draw()
     canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
     # Create animation
-    anim = FuncAnimation(fig, update_colors, fargs=(patches,), frames=1000, interval=500, blit=True)
+    anim = FuncAnimation(fig, update_colors, fargs=(patches,indexos,data), frames=1000, interval=100)
 
     # Start the GUI event loop
     root.mainloop()
 
 def main():
     data = read_csv(f"csv/PointBackupData_mod_2.csv")
+    #print(max(abs([ data[f'Volt_{i}'][j] for (i,j) in zip(range(100),range(1000))])))
     plot_animation(data)
    
 
