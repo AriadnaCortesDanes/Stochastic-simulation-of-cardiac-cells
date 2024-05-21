f2b4512a593b1ec962140951b67141edc67a3d60
arnaumarzabal <arnaumarzabal@gmail.com>
2024-05-21 08:24:48 +0200

    update Plot2d

--- Plot2d.py
+++ Plot2d.py
@@ -7,24 +7,27 @@ import matplotlib.animation as animation
 import pandas as pd
 
 
-df = pd.read_csv('csv/Voltages_40x40.csv')
-df = df.set_index('time_0_0')
+df = pd.read_csv('csv/TestQ1_20x20.csv',index_col=False)
+df = df.set_index('time')
 
 
 fps = 100
 nSeconds = 5
-snapshots = [ np.random.rand(5,5) for _ in range( nSeconds * fps ) ]
+
+matrix_shape = (20,20)
+
 
 
 # First set up the figure, the axis, and the plot element we want to animate
-fig = plt.figure( figsize=(40,40) )
+fig = plt.figure( figsize=matrix_shape )
 
-a = snapshots[0]
-im = plt.imshow(np.reshape(np.array(df.iloc[0]),(40,40)), interpolation='none', aspect='auto', vmin=-90, vmax=30)
+im = plt.imshow(np.reshape(np.array(df.iloc[0]),matrix_shape), interpolation='none', aspect='auto', vmin=-90, vmax=30)
+title = plt.title(f"Simulation T= {0} ms")
 
 def animate_func(i):
 
-    im.set_array(np.reshape(np.array(df.iloc[i]),(40,40)))
+    im.set_array(np.reshape(np.array(df.iloc[i]),matrix_shape))
+    title.set_text(f"Simulation T= {i*5} ms")
     return [im]
 
 anim = animation.FuncAnimation(
